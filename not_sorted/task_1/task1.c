f8ca1f2c5d5c5214de5d808a2c935673eec81f40
NikolasFRP <russiakremlin24@gmail.com>
2023-04-02 14:17:57 +0300

    in progress..

--- not_sorted/task_1/task1.c
+++ not_sorted/task_1/task1.c
@@ -3,7 +3,20 @@
 //
 #include <stdio.h>
 
+struct point{
+    int x;
+    int y;
+
+};
+struct point init(int x, int y){
+    struct point new;
+    new.x = x;
+    new.y = y;
+    return new;
+}
+
 int main(){
+    struct point p = init(5,5);
 
 
     return 0;
